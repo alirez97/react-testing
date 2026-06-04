8a79922913e02586951d6f4b4ed96de8474f3b31
Alireza <khanamani1998@gmail.com>
2026-06-04 13:10:35 +0330

    feat: add SearchBox tests.

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,7 @@
-import ExpandableText from '../components/ExpandableText';
+import SearchBox from '../components/SearchBox';
 
 const PlaygroundPage = () => {
-  return (
-    <ExpandableText text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis maxime laboriosam harum aliquam praesentium iste, fugit labore vel inventore, eveniet, aperiam quam dicta sint corrupti. Eveniet, laudantium non ab eum deserunt assumenda beatae, eaque accusantium neque minima saepe, molestiae aliquam. Delectus aperiam excepturi corrupti autem eos aut nulla voluptatum fugiat rerum, dolor suscipit quos iste dicta repellat laborum officia? Tempora, assumenda officiis vitae ratione esse quaerat a velit aliquam ad aliquid, fugit repellendus ea possimus quae doloribus architecto, quibusdam error atque optio asperiores quia praesentium voluptate tenetur totam. Dolores ad aut nihil dolore deleniti voluptatem magnam, sequi asperiores veritatis reprehenderit." />
-  );
+  return <SearchBox onChange={(text) => console.log(text)} />;
 };
 
 export default PlaygroundPage;
