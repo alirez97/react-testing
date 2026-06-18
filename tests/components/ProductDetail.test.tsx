b3b23db75efefcba0dccd038fe58bd52e2abcf1f
Alireza <khanamani1998@gmail.com>
2026-06-18 11:59:07 +0330

    refactor: using react query for product detail.

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -5,6 +5,7 @@ import {
 } from '@testing-library/react';
 import { delay, http, HttpResponse } from 'msw';
 import ProductDetail from '../../src/components/ProductDetail';
+import AllProviders from '../AllProviders';
 import { db } from '../mocks/db';
 import { server } from '../mocks/server';
 
@@ -21,7 +22,7 @@ describe('ProductDetail', () => {
   });
 
   it('should render an error for invalid product id', () => {
-    render(<ProductDetail productId={0} />);
+    render(<ProductDetail productId={0} />, { wrapper: AllProviders });
 
     expect(screen.getByText(/invalid/i)).toBeInTheDocument();
   });
@@ -29,7 +30,7 @@ describe('ProductDetail', () => {
   it('should render message if product not found', async () => {
     server.use(http.get('/products/:id', () => HttpResponse.json(null)));
 
-    render(<ProductDetail productId={1} />);
+    render(<ProductDetail productId={1} />, { wrapper: AllProviders });
 
     expect(await screen.findByText(/not found/i)).toBeInTheDocument();
   });
@@ -39,7 +40,7 @@ describe('ProductDetail', () => {
       where: { id: { equals: productId } },
     });
 
-    render(<ProductDetail productId={productId} />);
+    render(<ProductDetail productId={productId} />, { wrapper: AllProviders });
 
     const heading = await screen.findByRole('heading', {
       name: /product detail/i,
@@ -56,7 +57,7 @@ describe('ProductDetail', () => {
   it('should render an error message if data fetching fails', async () => {
     server.use(http.get('/products/:id', () => HttpResponse.error()));
 
-    render(<ProductDetail productId={productId} />);
+    render(<ProductDetail productId={productId} />, { wrapper: AllProviders });
 
     expect(await screen.findByText(/error/i)).toBeInTheDocument();
   });
@@ -73,13 +74,13 @@ describe('ProductDetail', () => {
       }),
     );
 
-    render(<ProductDetail productId={productId} />);
+    render(<ProductDetail productId={productId} />, { wrapper: AllProviders });
 
     expect(await screen.findByText(/loading/i)).toBeInTheDocument();
   });
 
   it('should remove loading indicator after data is fetched', async () => {
-    render(<ProductDetail productId={productId} />);
+    render(<ProductDetail productId={productId} />, { wrapper: AllProviders });
 
     await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
   });
@@ -87,7 +88,7 @@ describe('ProductDetail', () => {
   it('should remove loading indicator if data fetching fails', async () => {
     server.use(http.get('/products/:id', () => HttpResponse.error()));
 
-    render(<ProductDetail productId={productId} />);
+    render(<ProductDetail productId={productId} />, { wrapper: AllProviders });
 
     await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
   });
