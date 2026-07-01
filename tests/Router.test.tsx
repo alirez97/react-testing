77a037d631147473d4c3b7c2cc83164420e23ec8
Alireza <khanamani1998@gmail.com>
2026-07-01 14:18:05 +0330

    refactor: extract navigateTo function.

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -1,17 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import { createMemoryRouter, RouterProvider } from 'react-router-dom';
-import routes from '../src/routes';
+import { screen } from '@testing-library/react';
+import { navigateTo } from './utils';
 
 describe('Router', () => {
   it.each([
-    { page: 'home', heading: /home/i, route: '/' },
-    { page: 'products', heading: /products/i, route: '/products' },
-  ])('should render the $page page for $route', ({ heading, route }) => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: [route],
-    });
-
-    render(<RouterProvider router={router} />);
+    { page: 'home', heading: /home/i, path: '/' },
+    { page: 'products', heading: /products/i, path: '/products' },
+  ])('should render the $page page for $path', ({ heading, path }) => {
+    navigateTo(path);
 
     expect(screen.getByRole('heading', { name: heading })).toBeInTheDocument();
   });
